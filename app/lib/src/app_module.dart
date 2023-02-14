3b858e8d1379ea029eb8bfadad4b0a39d37c599a
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2023-02-13 21:46:30 -0300

    Adding data presintence with realm

--- app/lib/src/app_module.dart
+++ app/lib/src/app_module.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:listinha/src/configuration/services/configuration_services.dart';
 import 'package:listinha/src/home/home_module.dart';
+import 'package:listinha/src/shared/services/realm/realm_config.dart';
 import 'package:listinha/src/shared/stores/app_store.dart';
+import 'package:realm/realm.dart';
 import 'configuration/configuration_page.dart';
 
 class AppModule extends Module {
   @override
   List<Bind> get binds => [
+        Bind.instance<Realm>(Realm(config)),
+        AutoBind.factory<ConfigurationService>(ConfigurationServiceImpl.new),
         AutoBind.singleton(AppStore.new),
       ];
 
