16505354ad89f1f8e7376d57f9e8fb34f9192854
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2023-02-22 19:59:44 -0300

    Adding rx_notifier

--- app/lib/src/configuration/services/configuration_services.dart
+++ app/lib/src/configuration/services/configuration_services.dart
@@ -1,25 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:listinha/src/shared/services/realm/models/configuration_model.dart';
+import 'package:listinha/src/shared/stores/app_store.dart';
 import 'package:realm/realm.dart';
+import 'package:rx_notifier/rx_notifier.dart';
 
 abstract class ConfigurationService {
-  ConfigurationModel getConfiguration();
-  void saveConfiguration(String themeModeName, DateTime? syncDate);
+  void init();
   void deleteAll();
 }
 
-class ConfigurationServiceImpl implements ConfigurationService {
+class ConfigurationServiceImpl implements ConfigurationService, Disposable {
   final Realm realm;
+  final AppStore appStore;
+  late final RxDisposer disposer;
 
-  ConfigurationServiceImpl(this.realm);
+  ConfigurationServiceImpl(this.realm, this.appStore);
 
   @override
-  ConfigurationModel getConfiguration() {
+  void init() {
+    final model = _getConfiguration();
+    appStore
+      ..themeMode = _getThemeModeByName(model.themeModeName)
+      ..syncDate = model.syncDate;
+
+    disposer = rxObserver(() {
+      final themeMode = appStore.themeMode;
+      final syncDate = appStore.syncDate;
+      _saveConfiguration(themeMode.name, syncDate);
+    });
+  }
+
+  @override
+  void deleteAll() {
+    realm.deleteAll();
+  }
+
+  @override
+  void dispose() {
+    dispose();
+  }
+
+  @override
+  ConfigurationModel _getConfiguration() {
     return realm.all<ConfigurationModel>().first;
   }
 
   @override
-  void saveConfiguration(String themeModeName, DateTime? syncDate) {
-    final model = getConfiguration();
+  void _saveConfiguration(String themeModeName, DateTime? syncDate) {
+    final model = _getConfiguration();
     realm.write(
       () {
         model
@@ -29,8 +58,7 @@ class ConfigurationServiceImpl implements ConfigurationService {
     );
   }
 
-  @override
-  void deleteAll() {
-    realm.deleteAll();
+  ThemeMode _getThemeModeByName(String name) {
+    return ThemeMode.values.firstWhere((mode) => mode.name == name);
   }
 }
